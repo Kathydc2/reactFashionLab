53ef82f1c249e923d905786d5056dcfdb9bb94ed
kathy Del Castillo <kathydcastillo90@gmail.com>
2024-05-04 21:20:07 -0400

    final project

--- react-fashion-lab/src/App.js
+++ react-fashion-lab/src/App.js
@@ -13,8 +13,18 @@ function App() {
     <div className="App">
       <Header />
       <Nav nav={navOptions} />
-      <Article />
-      <hr></hr>
+      <Article 
+      date="11/12/20"
+      title="On the Street in Brooklyn"
+      imageSrc="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-1.jpg"
+      letter="C"/>
+      
+      <Article 
+      date="11/11/20"
+      title="Vintage in Vogue"
+      imageSrc="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-2.jpg"
+      letter="S"/>
+      
       <Footer nav={navOptions} />
    
     </div>
--- react-fashion-lab/src/components/Article.jsx
+++ react-fashion-lab/src/components/Article.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function Article() {
+function Article({date, title, imageSrc, letter}) {
   return (
     <div className='article'>
+      <div className="date">{date}</div>
+      <div className="title">{title}</div>
+      <img className="image" src={imageSrc} alt="" />
       
+      <p className='content'><p className='letter'>{letter}</p> Dolor sit amet consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis pripsum por. Praesent mauris. Fusce nec tellus sed augue semper porta. 
+        Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad imperdieter torquent per conubia nostra, per inceptos himenaeos.Curabitur sodales ligula in libero.Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui amet iusto natus expedita tenetur quas illo, quasi eos possimus facilis minus magnam aperiam cumque quae deserunt, temporibus voluptate veritatis quibusdam!sit amet consectetur adipisicing Qui amet aportent per inceptos himenaeos Lorem ipsum dolor.</p>
+  
+      
+        <p className='continue'>Continues...</p> 
+        <hr></hr>
+       
       </div>
   )
 }
